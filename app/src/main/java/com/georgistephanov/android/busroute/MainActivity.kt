1436beb58a96929a7de01310c6bc81d4cd8cce31
Georgi Stefanov <georgistephanov@gmail.com>
2018-02-17 12:55:01 +0000

    Initial layout

--- app/src/main/java/com/georgistephanov/android/busroute/MainActivity.kt
+++ app/src/main/java/com/georgistephanov/android/busroute/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.georgistephanov.android.busroute
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_camera -> {
+                Toast.makeText(this, "Open the camera", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return true
     }
 }
